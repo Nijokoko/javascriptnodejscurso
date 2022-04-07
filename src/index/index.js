40435981a796088a179a74785012abc974bec17b
nicolas navarrete <nico30.asd@gmail.com>
2022-04-07 19:58:16 -0400

    actualizado

--- src/index/index.js
+++ src/index/index.js
@@ -1,30 +1,29 @@
 const express = require('express');
+const {Response} = require('../common/response');
 const createError = require('http-errors');
 
-const { Response } = require('../common/response');
-
-module.exports.IndexAPI= (app) => {
+module.exports.IndexAPI = (app) =>{
     const router = express.Router();
+    router.get("/", (req,res) => {
 
-    router.get("/", (req, res) => {
         const menu = {
             products: `https://${req.headers.host}/api/products`,
-            users: `https://${req.headers.host}/api/users`,
+            users: `https://${req.headers.host}/api/user`,
             sales: `https://${req.headers.host}/api/sales`
-        };
+        }
 
-        Response.success(res, 200, "API Inventario", {})
+        Response.success(res,200,"API Inventario",menu);
     });
 
-    app.use("/", router);
-};
-
-module.exports.NotFoundAPI= (app) => {
+    app.use("/",router);
+}
+module.exports.NotFoundAPI = (app) =>{
     const router = express.Router();
-
-    router.all("*", (req,res) => {
+    
+    router.all("*",(req,res)=>{
         Response.error(res, new createError.NotFound());
-    });
+    })
 
-    app.use("/", router);
-};
+    app.use("/",router);
+}
+
